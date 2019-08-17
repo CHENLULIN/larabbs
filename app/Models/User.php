9c26234abcd820a00227e74d5d8cca2696acc882
林宇东 <306073100@qq.com>
2019-08-17 04:04:26 +0000

    用户话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
     	return $this->hasMany(Expenditure::class,'user_id');
     }
+    
+    public function topics()
+    {
+    	return $this->hasMany(Topic::class, 'user_id');
+    }
 }
