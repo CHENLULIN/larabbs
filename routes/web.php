575bbe1d59efe8479a8b34e81d674e0a7b485e38
林宇东 <306073100@qq.com>
2019-08-19 14:10:12 +0000

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,9 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 Route::resource('expenditures', 'ExpendituresController');
 
 // 话题资源路由
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 //分类资源路由
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
