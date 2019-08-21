27f352074bffbfaa46f917a42fa78d7028171fab
林宇东 <306073100@qq.com>
2019-08-21 04:03:37 +0000

    消息通知的表

--- routes/web.php
+++ routes/web.php
@@ -51,5 +51,8 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 //图片上传路由
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
-
+//评论路由
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+
+// 通知路由
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
